2503f56e778a4461330185782a010319f1fbd5c4
Alexeev <sergey50878@mail.ru>
2025-10-22 21:05:13 +0300

    init

---
+++ src/main/java/org/global/handtalk/BotHandTalkerApplication.java
@@ -0,0 +1,13 @@
+package org.global.handtalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotHandTalkerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BotHandTalkerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/global/handtalk/BotHandTalkerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.global.handtalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotHandTalkerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
